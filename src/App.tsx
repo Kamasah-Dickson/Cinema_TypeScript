90381cb66241b2f0d550d18a76747007daa2a58a
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-01-14 23:43:12 +0000

    add essentials

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.css";
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Trending from "./components/Trending";
+import TopRated from "./components/TopRated";
+import ContinueWatching from "./components/ContinueWatching";
 
 const App: React.FC = () => {
-	return <p>Hello</p>;
+	return (
+		<>
+			<Header />
+			<main className="main">
+				<Trending />
+				<TopRated />
+				<ContinueWatching />
+				<Footer />
+			</main>
+			<aside className="aside"></aside>
+		</>
+	);
 };
 
 export default App;
