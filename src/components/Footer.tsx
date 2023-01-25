3d0447dc242b116db97ba501150372b89a718f26
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-01-25 22:38:11 +0000

    add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BsInstagram } from "react-icons/bs";
+import { FiTwitter } from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa";
+import { ImGithub } from "react-icons/im";
 
-function Footer() {
-	return <div>Footer</div>;
-}
+const Footer: React.FC = () => {
+	return (
+		<div className="footer-section">
+			<div className="container">
+				<div className="icons">
+					<BsInstagram />
+					<FiTwitter />
+					<FaLinkedin />
+					<ImGithub />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Footer;
