b16d6fc55594966878dd2054f9e40f45d416f4d6
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-01-28 06:30:05 +0000

    update stateContext

--- src/context/StateContext.tsx
+++ src/context/StateContext.tsx
@@ -8,19 +8,25 @@ import React, {
 export interface toggleInterface {
 	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 	setTheme: React.Dispatch<React.SetStateAction<boolean>>;
+	setSearch: React.Dispatch<React.SetStateAction<boolean>>;
 	open: boolean;
 	theme: boolean;
+	searchMovie: boolean;
 }
 
 export const contextProvider = createContext<toggleInterface>({
-	open: false,
 	setOpen: () => {},
-	theme: true,
 	setTheme: () => {},
+	setSearch: () => {},
+	open: false,
+	theme: true,
+	searchMovie: false,
 });
 
 function StateContext({ children }: PropsWithChildren) {
 	const [open, setOpen] = useState<boolean>(false);
+	const [searchMovie, setSearch] = useState<boolean>(false);
+
 	const [theme, setTheme] = useState<boolean>(
 		JSON.parse(localStorage.getItem("theme") || (true as any))
 	);
@@ -30,7 +36,9 @@ function StateContext({ children }: PropsWithChildren) {
 	}, [theme]);
 
 	return (
-		<contextProvider.Provider value={{ open, setOpen, theme, setTheme }}>
+		<contextProvider.Provider
+			value={{ open, setOpen, theme, setTheme, searchMovie, setSearch }}
+		>
 			{children}
 		</contextProvider.Provider>
 	);
